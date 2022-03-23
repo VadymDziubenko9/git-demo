84edd6ceb37c198d3bbe61400ebaf984e96520bd
Vadym Dziubenko <vadymdziubenko99@gmail.com>
2022-03-23 17:14:33 +0200

    create a repository

---
+++ bin/calculator.dart
@@ -0,0 +1,11 @@
+
+
+import 'dart:io';
+
+void main(){
+  int n1, n2 , userChoice;
+  stdout.write('Enter first number :');
+  n1 = int.parse(stdin.readLineSync()!);
+  stdout.write('Enter second number :');
+  n2 = int.parse(stdin.readLineSync()!);
+}
