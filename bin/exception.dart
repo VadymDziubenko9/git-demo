5a477d4b17cd70e2bb365de2df5777a146fb4ba8
Vadym Dziubenko <vadymdziubenko99@gmail.com>
2022-04-06 00:03:57 +0300

    class addition

---
+++ bin/exception.dart
@@ -0,0 +1,49 @@
+class DivisionByZero {
+  @override
+  String toString() {
+    return 'Forbidden division by zero';
+  }
+}
+
+class IncorrectIntegerString {
+  final String incorrectString;
+
+  IncorrectIntegerString(this.incorrectString);
+
+  @override
+  String toString() {
+    return 'incorrect value: $incorrectString';
+  }
+}
+
+double? division(String a, String b) {
+  final aa = int.tryParse(a);
+  final bb = int.tryParse(b);
+
+  if (bb == 0) {
+    throw DivisionByZero();
+  }
+
+  if (aa == null) {
+    throw IncorrectIntegerString(a);
+  }
+
+  if (bb == null) {
+    throw IncorrectIntegerString(b);
+  }
+
+  return aa / bb;
+}
+
+void main() {
+  try {
+    final div = division('0f', '0');
+    print(div);
+  } on DivisionByZero catch (error) {
+    print(error);
+  } on IncorrectIntegerString catch (error) {
+    print(error);
+  } catch (error) {
+    print(error);
+  }
+}
